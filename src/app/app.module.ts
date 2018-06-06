a4bbf0468a1d2b3c5a1139a4133e9b35577a88ae
Martijn Klene <martijn.klene@voiceworks.com>
2018-06-06 16:13:33 +0200

    Angular universal starting to behave itself, next implementing the service to retrieve blog posts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+
 @NgModule({
     declarations: [
         AppComponent
@@ -13,5 +17,13 @@ import {AppComponent} from './app.component';
     providers: [],
     bootstrap: [AppComponent]
 })
+
 export class AppModule {
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: Object,
+        @Inject(APP_ID) private appId: string) {
+        const platform = isPlatformBrowser(platformId) ?
+            'in the browser' : 'on the server';
+        console.log(`Running ${platform} with appId=${appId}`);
+    }
 }
