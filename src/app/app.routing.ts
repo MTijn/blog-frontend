7218289f32dc2b428bce8c5f399c366b80af6850
Martijn Klene <martijn.klene@voiceworks.com>
2019-02-11 10:53:22 +0100

    Remove the not found component for the moment.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {HomeComponent} from './component/home/HomeComponent';
 import {ArchiveComponent} from './component/archive/archive.component';
-import {NotFoundComponent} from './component/NotfoundComponent';
 
 export const AppRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'archive', component: ArchiveComponent},
-    { path: '404', component: NotFoundComponent},
-    { path: '**', redirectTo: '404'}
+    { path: 'archive', component: ArchiveComponent}
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
