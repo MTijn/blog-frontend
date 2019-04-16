66756da9f744e823dc69c735fe1cc71d74e4caa8
M.Tijn <schapie.nl@gmail.com>
2019-04-16 18:54:21 +0200

    Added the ld-json to the detail too, and use ng lint.

--- src/app/component/detail/DetailComponent.ts
+++ src/app/component/detail/DetailComponent.ts
@@ -5,7 +5,7 @@ import {BlogService} from '../../service/BlogService';
 import {Observable} from 'rxjs/internal/Observable';
 
 @Component({
-    selector: 'detail',
+    selector: 'app-detail',
     templateUrl: 'DetailComponent.html'
 })
 export class DetailComponent implements OnInit {
@@ -23,5 +23,23 @@ export class DetailComponent implements OnInit {
 
     getBlogPost(): void {
         this.blogPost = this.blogService.fetchSingleBlogPostByIdentifier(this.route.snapshot.paramMap.get('id'));
+        this.blogPost.toPromise().then(blogPost => {
+                this.blogPostSchema = {
+                    '@context': 'http://schema.org',
+                    '@type': 'BlogPosting',
+                    'headline': blogPost.title,
+                    'datePublished': blogPost.publishedAt,
+                    'keywords': blogPost.tags,
+                    'url': 'https://martijnklene.nl',
+                    'author': blogPost.author,
+                    'publisher': {
+                        '@type': 'Organization',
+                        'name': 'KlenePublishing',
+                        'logo': 'https://pbs.twimg.com/profile_images/998841428238262274/g71Qp9j2_400x400.jpg'
+                    },
+                    'image': 'https://pbs.twimg.com/profile_images/998841428238262274/g71Qp9j2_400x400.jpg'
+                };
+            }
+        );
     }
 }
