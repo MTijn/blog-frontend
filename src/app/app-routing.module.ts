b5438ed6a906fed85f030a89de820aee8d249dd8
Martijn Klene <martijn.klene@enreach.com>
2022-11-23 09:12:34 +0100

    Changed the package versions, and migrated to Angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledNonBlocking',
-    relativeLinkResolution: 'legacy'
+    initialNavigation: 'enabledNonBlocking'
 })],
   exports: [RouterModule]
 })
