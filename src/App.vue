ce724255fa7abeaf3a082b88967d6198cbfa1d56
Martijn Klene <martijn.klene@enreach.com>
2023-02-28 11:41:57 +0100

    Making a header.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar
-        title="MartijnKlene.nl"
-        class="px-3"
-        color="white"
-        flat
-        density="compact"
-    >
-      <v-tabs color="grey-darken-2">
-        <v-tab to="/">Home</v-tab>
-        <v-tab to="/about">About</v-tab>
+    <v-app-bar>
+      <v-tabs>
+        <v-tab to="/"><h1>MartijnKlene.nl</h1></v-tab>
+        <v-tab to="/archive">Archive</v-tab>
       </v-tabs>
     </v-app-bar>
     <v-main>
@@ -20,5 +14,4 @@
   </v-app>
 </template>
 <script setup lang="ts">
-import * as path from "path";
 </script>
