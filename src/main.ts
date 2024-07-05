bfd2410a9a987ed47eef9fde8bf8e27b30aaf359
Martijn Klene <martijn.klene@enreach.com>
2024-07-05 11:40:57 +0200

    Adding nicer style around quotes.

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "fork-awesome/css/fork-awesome.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "@popperjs/core/dist/esm/popper.js"
+import "./assets/blockquote.css"
 
 createApp(App).use(router).mount('#app')
